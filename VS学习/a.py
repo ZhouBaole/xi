eca2e7e635e228bb1dbc5c129c24cfd4c8d0d712
周保乐 <1542530095@qq.com>
2020-06-11 12:22:02 +0800

    添加h

---
+++ VS学习/a.py
@@ -0,0 +1,24 @@
+print("asdad")
+"""
+循环
+"""
+#whil循环
+k=0
+while k<9:   
+    print("循环中:",k)  
+    #if语句
+    if k==7:
+        print(k) 
+    k=k+1 
+else:
+    print("循环结束")
+    pass
+
+#数组
+a=[{"asd":1,"dsf":"asd","dfsgdfg":"sadasd"},{"asd":2,"dsf":"asd2","dfsgdfg":"sadasd2"}]
+#for循环
+for d in a:
+    print(d)
+
+for i in range(-10, -130, -30) :
+    print(i)
